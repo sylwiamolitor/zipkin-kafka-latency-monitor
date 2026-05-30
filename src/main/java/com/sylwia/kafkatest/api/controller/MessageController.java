7deecc70aa607788c7123f34e9b7834c4bdd2317
Sylwia <sylwiamlr@gmail.com>
2026-05-30 18:31:19 +0200

    feat: add Elasticsearch search by message field

--- src/main/java/com/sylwia/kafkatest/api/controller/MessageController.java
+++ src/main/java/com/sylwia/kafkatest/api/controller/MessageController.java
@@ -1,27 +1,35 @@
 package com.sylwia.kafkatest.api.controller;
 
 import com.sylwia.kafkatest.api.dto.Message;
+import com.sylwia.kafkatest.api.repository.MessageRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/messages")
 @Tag(name = "Operations on messages.")
 public class MessageController {
-    private KafkaTemplate<String, String> kafkaTemplate;
 
-    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final MessageRepository messageRepository;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate, MessageRepository messageRepository) {
         this.kafkaTemplate = kafkaTemplate;
+        this.messageRepository = messageRepository;
     }
 
     @PostMapping
     @Operation(summary = "Method for publishing messages.")
     public void publish(@RequestBody Message request) {
         kafkaTemplate.send("sylwia", request.getMessage());
+    }
+
+    @GetMapping("/search")
+    public List<Message> search(@RequestParam String query) {
+        return messageRepository.search(query);
     }
 }
--- src/main/java/com/sylwia/kafkatest/api/repository/MessageRepository.java
+++ src/main/java/com/sylwia/kafkatest/api/repository/MessageRepository.java
@@ -1,7 +1,14 @@
 package com.sylwia.kafkatest.api.repository;
 
 import com.sylwia.kafkatest.api.dto.Message;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends ElasticsearchRepository<Message, String> {
+
+    @Query("{\"match\": {\"message\": \"?0\"}}")
+    List<Message> search(String text);
+
 }
